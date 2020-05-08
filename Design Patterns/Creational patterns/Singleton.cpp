1ba1eb7f5788e03df99a88b79e604492b92d8d28
Areg-Abgaryan <61087025+Areg-Abgaryan@users.noreply.github.com>
2020-05-08 17:15:29 +0400

    Add files via upload

---
+++ Design Patterns/Creational patterns/Singleton.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Russia_President
+{
+private:
+	string name = "Putin";
+protected:
+	Russia_President() = default;
+	~Russia_President() = default;
+public:
+	Russia_President(const Russia_President&) = delete;
+	Russia_President& operator=(const Russia_President&) = delete;
+
+	Russia_President(const Russia_President&&) noexcept = delete;
+	Russia_President& operator=(const Russia_President&&) noexcept = delete;
+
+	string get_value() const
+	{
+		return name;
+	}
+
+	void send_president_to_vacation()
+	{
+		name = "Medvedev";
+		cout << "\nMaybe for 4 years only.\n";
+	}
+
+	static Russia_President& getinstance()
+	{
+		static Russia_President object;
+		return object;
+	}
+};
+
+int main()
+{
+	Russia_President &rp = Russia_President::getinstance();
+	rp.send_president_to_vacation();
+	cout << rp.get_value();
+}
