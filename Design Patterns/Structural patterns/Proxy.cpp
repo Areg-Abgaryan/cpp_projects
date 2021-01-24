973b5f03150cc8b041cec110acebdd0f4406d531
Areg-Abgaryan <abgaryan.areg@gmail.com>
2021-01-24 19:18:14 +0400

    Remove "using namespace std"

--- Design Patterns/Structural patterns/Proxy.cpp
+++ Design Patterns/Structural patterns/Proxy.cpp
@@ -2,12 +2,10 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
 class FBI_Archives
 {
 public:
-	virtual string get_data() const = 0;
+	virtual std::string get_data() const = 0;
 	virtual ~FBI_Archives() {}
 };
 
@@ -19,12 +17,13 @@ class FBI_Agent
 private:
 	friend class FBI_Database;
 
-	string name, surname, birth_year;
-	string address, passport_id;
-	string password;
-	vector<string> info;
-	
+	std::string name, surname, birth_year;
+	std::string address, passport_id;
+	std::string password;
+	std::vector<std::string> info;
+
 	FBI_Agent() = default;
+
 public:
 	~FBI_Agent() = default;
 };
@@ -34,11 +33,11 @@ class FBI_Database : FBI_Archives
 private:
 	friend class FBI_Database_Access;
 
-	vector<string> secret_data{ "Imagine a huuuge amount of data here" };
-	vector<FBI_Agent> agents;
+	std::vector<std::string> secret_data{ "Imagine a huuuge amount of data here" };
+	std::vector<FBI_Agent> agents;
 
 	FBI_Database() = default;
-	
+
 	FBI_Database(const FBI_Database&) = delete;
 	FBI_Database& operator=(const FBI_Database&) = delete;
 	FBI_Database(const FBI_Database&&) = delete;
@@ -50,20 +49,21 @@ private:
 		return object;
 	}
 
-	string get_data() const override
+	std::string get_data() const override
 	{
-		cout << "Real db\n";
-		string s = secret_data[rand()%secret_data.size()];
+		std::cout << "Real db\n";
+		std::string s = secret_data[rand() % secret_data.size()];
 		return s;
 	}
 
 	~FBI_Database() = default;
 };
 
-class FBI_Database_Access: public FBI_Archives
+class FBI_Database_Access : public FBI_Archives
 {
 private:
 	FBI_Database &dbinst = FBI_Database::get_instance();
+
 public:
 	FBI_Database_Access() = default;
 
@@ -72,9 +72,9 @@ public:
 	FBI_Database_Access(const FBI_Database_Access&&) = delete;
 	FBI_Database_Access& operator=(const FBI_Database_Access&&) = delete;
 
-	string get_data() const override
+	std::string get_data() const override
 	{
-		cout << "Proxy access\n";
+		std::cout << "Proxy access\n";
 		return dbinst.get_data();
 	}
 
@@ -83,6 +83,6 @@ public:
 
 int main()
 {
-	FBI_Database_Access hacker;
+	FBI_Database_Access hacker{};
 	hacker.get_data();
 }
