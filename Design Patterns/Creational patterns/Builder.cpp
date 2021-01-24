f96c48bec63bf559f2f3281f3960d86a9f358ba4
Areg-Abgaryan <abgaryan.areg@gmail.com>
2021-01-24 18:33:58 +0400

    Remove "using namespace std"

--- Design Patterns/Creational patterns/Builder.cpp
+++ Design Patterns/Creational patterns/Builder.cpp
@@ -2,16 +2,14 @@
 #include <string>
 #include <memory>
 
-using namespace std;
-
 class Movie_Character
 {
 private:
-	string street_address{}, city{}, company_name{}, position{};
+	std::string street_address{}, city{}, company_name{}, position{};
 	int annual_income = 0, post_code = 0;
 protected:
-	Movie_Character(const string& _street_address, const int _post_code, const string& _city,
-		const string& _company_name, const string& _position, const int _annual_income) :
+	Movie_Character(const std::string& _street_address, const int _post_code, const std::string& _city,
+		const std::string& _company_name, const std::string& _position, const int _annual_income) :
 		street_address{ _street_address }, post_code{ _post_code }, city{ _city },
 		company_name{ _company_name }, position{ _position }, annual_income{ _annual_income } {}
 public:
@@ -26,22 +24,22 @@ public:
 class Movie_Character_Builder
 {
 protected:
-	unique_ptr<Movie_Character> person{ nullptr };
-	Movie_Character_Builder() { person = make_unique<Movie_Character>(); }
+	std::unique_ptr<Movie_Character> person{ nullptr };
+	Movie_Character_Builder() { person = std::make_unique<Movie_Character>(); }
 public:
-	unique_ptr<Movie_Character> getCharacter()
+	std::unique_ptr<Movie_Character> getCharacter()
 	{
 		return move(person);
 	}
 
 	Movie_Character_Builder(Movie_Character_Builder&&) = default;
 
-	virtual void at(const string& street_address) {}
+	virtual void at(const std::string& street_address) {}
 	virtual void post_code(const int post_code) {}
-	virtual void in(const string& city) {}
+	virtual void in(const std::string& city) {}
 
-	virtual void company(const string& company_name) {}
-	virtual void as_a(const string& position) {}
+	virtual void company(const std::string& company_name) {}
+	virtual void as_a(const std::string& position) {}
 	virtual void salary(const int annual_income) {}
 };
 
@@ -50,7 +48,7 @@ class Movie_Character_Address_Builder : public Movie_Character_Builder
 public:
 	Movie_Character_Address_Builder() = default;
 
-	void at(const string& street_address) override
+	void at(const std::string& street_address) override
 	{
 		person->street_address = street_address;
 	}
@@ -58,7 +56,7 @@ public:
 	{
 		person->post_code = post_code;
 	}
-	void in(const string& city) override
+	void in(const std::string& city) override
 	{
 		person->city = city;
 	}
@@ -71,11 +69,11 @@ class Movie_Character_Job_Builder : public Movie_Character_Builder
 public:
 	Movie_Character_Job_Builder() = default;
 
-	void company(const string& company_name) override
+	void company(const std::string& company_name) override
 	{
 		person->company_name = company_name;
 	}
-	void as_a(const string& position) override
+	void as_a(const std::string& position) override
 	{
 		person->position = position;
 	}
@@ -90,14 +88,14 @@ public:
 class Director
 {
 private:
-	unique_ptr<Movie_Character_Builder> builder{ nullptr };
+	std::unique_ptr<Movie_Character_Builder> builder{ nullptr };
 public:
-	Director(unique_ptr<Movie_Character_Builder>& b)
+	Director(std::unique_ptr<Movie_Character_Builder>& b)
 	{
-		builder = move(b);
+		builder = std::move(b);
 	}
 
-	unique_ptr<Movie_Character> createCharacter()
+	std::unique_ptr<Movie_Character> createCharacter()
 	{
 		builder->at("Wollestraat 41-47");
 		builder->post_code(8310);
@@ -111,12 +109,12 @@ public:
 
 int main()
 {
-	unique_ptr<Movie_Character_Builder> builder1 = make_unique<Movie_Character_Address_Builder>();
-	unique_ptr<Movie_Character_Builder> builder2 = make_unique<Movie_Character_Job_Builder>();
+	std::unique_ptr<Movie_Character_Builder> builder1 = std::make_unique<Movie_Character_Address_Builder>();
+	std::unique_ptr<Movie_Character_Builder> builder2 = std::make_unique<Movie_Character_Job_Builder>();
 
-	unique_ptr<Director> eng = make_unique<Director>(builder1);
+	std::unique_ptr<Director> eng = std::make_unique<Director>(builder1);
 	eng->createCharacter();
 
-	eng = make_unique<Director>(builder2);
+	eng = std::make_unique<Director>(builder2);
 	eng->createCharacter();
 }
